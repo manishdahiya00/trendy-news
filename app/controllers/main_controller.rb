feb4a800f5a9ac72fd7a6aa4fe5402847e3ab5fe
Manish Dahiya <manishdahiya0944@gmail.com>
2024-08-28 12:53:14 +0530

    delete user

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,10 @@
 class MainController < ApplicationController
   def index
+    if params[:deleteuser].present?
+      @user = User.find_by(social_email: params[:deleteuser])
+      if @user
+        @user.destroy
+      end
+    end
   end
 end
